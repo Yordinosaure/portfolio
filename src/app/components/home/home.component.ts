960315d6aabb8bf65f3cdafba0890c96f316c630
Yordi David <yordinosaure@gmail.com>
2020-07-23 22:13:08 +0200

    test logger

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoggerService } from 'src/app/core/services/logger.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  
+  test: number = 4;
+  
+  constructor(private logger: LoggerService) { }
 
   ngOnInit(): void {
+    this.logger.log(5);
+    this.logger.log(this.test, 'test', 'green');    
   }
 
 }
