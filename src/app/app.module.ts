8a9a454cd3fbf58e9b090d5c6abe9d704c5fcce4
Yordi David <yordinosaure@gmail.com>
2020-08-05 14:32:06 +0200

    import three test comp to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
+import { ThreeTestComponent } from './components/three-test/three-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ThreeTestComponent
   ],
   imports: [
     BrowserModule,
