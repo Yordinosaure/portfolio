1906d4f7c47d30c1557b988aedf2b137bbe7bc2b
Yordi David <yordinosaure@gmail.com>
2020-08-06 13:54:36 +0200

    content service

---
+++ src/app/core/services/content/content.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContentService {
+
+  menuContent = [
+    {cssId: 'who', title: 'who am i'},
+    {cssId: 'experience', title: 'experience'},
+    {cssId: 'hard', title: 'hard skills'},
+    {cssId: 'soft', title: 'soft skills'},
+    {cssId: 'contact', title: 'contact me'},
+  ]
+
+  constructor() { }
+
+  getMenuContent() {
+    return this.menuContent;
+  }
+}
