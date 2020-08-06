ddf03f9623e52929c3623b52f2fe760be369488c
Yordi David <yordinosaure@gmail.com>
2020-08-06 18:34:59 +0200

    menu animation

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ContentService } from 'src/app/core/services/content/content.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('menuState', [
+      state('show', style({
+        opacity: 1,
+        transform: 'translateY(0)'
+      })),
+      state('hide', style({
+        opacity: 0,
+        transform: 'translateY(-50px)',
+        display:'none'
+      })),
+      transition('show => hide', animate('600ms ease-out')),
+      transition('hide => show', animate('600ms ease-in'))
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
 
   toggled = "";
+  isMenuDisplayed = false;
   contents = [];
   constructor(private contentService: ContentService) { }
 
@@ -16,9 +33,17 @@ export class HeaderComponent implements OnInit {
     this.contents = this.contentService.getMenuContent();
   }
 
-  toggle(str) {
+  toggleMenuItem(str) {
     console.log(str)
     this.toggled = str;
+  }
+
+  get stateName() {
+    return this.isMenuDisplayed ? 'show':'hide';
+  }
+
+  displayMenu() {
+    this.isMenuDisplayed = !this.isMenuDisplayed;
   }
 
 }
