e44bf984e5bef0c5f02fcf5690ad5bcb44aceac8
Yordi David <yordinosaure@gmail.com>
2020-08-11 16:46:43 +0200

    all not needed anymore commented in homecomp

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,23 +10,23 @@ import { ThreeService } from 'src/app/core/services/three/three.service';
 })
 export class HomeComponent implements OnInit {
 
-  @ViewChild('renderer', {static: true})
-  rendererCanvas: ElementRef<HTMLDivElement>;
+  // @ViewChild('renderer', {static: true})
+  // rendererCanvas: ElementRef<HTMLDivElement>;
 
-  @HostListener('window:resize')
-  onResize(){
-    // this.threeService.resizeAction();
-  }
+  // @HostListener('window:resize')
+  // onResize(){
+  //   // this.threeService.resizeAction();
+  // }
 
-  @HostListener('mousemove', ['$event'])
-  onMouseDrag(event){
-    // this.threeService.onMouseDragAction(event);
-  }
+  // @HostListener('mousemove', ['$event'])
+  // onMouseDrag(event){
+  //   // this.threeService.onMouseDragAction(event);
+  // }
 
-  @HostListener('wheel', ['$event']) // for window scroll events
-  onScroll(event) {
-    // this.threeService.zoom(event.deltaY);
-  }
+  // @HostListener('wheel', ['$event']) // for window scroll events
+  // onScroll(event) {
+  //   // this.threeService.zoom(event.deltaY);
+  // }
 
   
   
