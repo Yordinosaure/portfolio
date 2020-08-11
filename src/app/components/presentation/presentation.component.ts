f0f0529a8294fb3bd3e3ec5bacd1a76737f67047
Yordi David <yordinosaure@gmail.com>
2020-08-11 15:42:09 +0200

    presentation style and content fix

--- src/app/components/presentation/presentation.component.ts
+++ src/app/components/presentation/presentation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentService } from 'src/app/core/services/content/content.service';
 
 @Component({
   selector: 'app-presentation',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresentationComponent implements OnInit {
 
-  constructor() { }
+  content: any;
+
+  constructor(private contentS: ContentService) { }
 
   ngOnInit(): void {
+    this.content = this.contentS.getPresentationContent();
   }
 
 }
