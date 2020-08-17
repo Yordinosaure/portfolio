d13b2f869e733297a3ac8888f252f42003aef47f
Yordi David <yordinosaure@gmail.com>
2020-08-17 16:35:29 +0200

    hard skill checkpoint

--- src/app/components/hard-skills/hard-skills.component.ts
+++ src/app/components/hard-skills/hard-skills.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { runInThisContext } from 'vm';
+import { ThreeDService } from 'src/app/core/services/three-d.service';
+import { CheckPoint } from 'src/app/core/models/checkPoints.model';
 
 @Component({
   selector: 'app-hard-skills',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HardSkillsComponent implements OnInit {
 
-  constructor() { }
+  startY: number;
+  checkPoint: CheckPoint;
+
+  constructor(private threeDService: ThreeDService) { }
 
   ngOnInit(): void {
+    this.startY = document.getElementById('startHard').offsetTop;
+    console.log('hard start point',this.startY);
+    this.checkPoint = new CheckPoint('hard', this.startY, 0, 0, 0, 0);
+    this.threeDService.addCheckPoint(this.checkPoint);
   }
 
 }
