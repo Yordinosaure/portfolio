718ed315ba01b06d2031eaf3110b6368fd3bd67f
Yordi David <yordinosaure@gmail.com>
2020-08-17 16:36:38 +0200

    presentation checkpoint

--- src/app/components/presentation/presentation.component.ts
+++ src/app/components/presentation/presentation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ContentService } from 'src/app/core/services/content/content.service';
+import { ThreeDService } from 'src/app/core/services/three-d.service';
+import { CheckPoint } from 'src/app/core/models/checkPoints.model';
 
 @Component({
   selector: 'app-presentation',
@@ -9,11 +11,14 @@ import { ContentService } from 'src/app/core/services/content/content.service';
 export class PresentationComponent implements OnInit {
 
   content: any;
+  checkPoint: CheckPoint
 
-  constructor(private contentS: ContentService) { }
+  constructor(private contentS: ContentService, private threeDService: ThreeDService) { }
 
   ngOnInit(): void {
     this.content = this.contentS.getPresentationContent();
+    this.checkPoint = new CheckPoint('presentation', 0, 0, 0, 0, 0);
+    this.threeDService.addCheckPoint(this.checkPoint);
   }
 
 }
