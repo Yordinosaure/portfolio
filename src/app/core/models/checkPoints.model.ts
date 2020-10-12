6015653912f9182d356bc6b6d07c4a320073c884
Yordi David <yordinosaure@gmail.com>
2020-10-12 15:11:01 +0200

    checkpoints to keep?

--- src/app/core/models/checkPoints.model.ts
+++ src/app/core/models/checkPoints.model.ts
@@ -1,27 +1,35 @@
+import { ThreeDService } from '../services/three-d.service';
+import * as Three from 'three';
+
 export class CheckPoint {
 
     cssId: string;
-    docPosition: number;
-    destinationV: V3;
+    docStartPosition: number;
+    docEndPosition: number;
+    // destinationV: V3;
+    destinationV: Three.Vector3
     destinationAngleDeg: number;
     angleRad: number;
 
-    constructor(id: string, docPosY: number, destinationX: number, destinationY: number, destinationZ:number, angleDeg: number){
+    constructor(id: string, docStartPosY: number, endPositionY: number, destinationX: number, destinationY: number, destinationZ:number, angleDeg: number){
         this.cssId = id;
-        this.docPosition = docPosY;
-        this.destinationV = new V3(destinationX, destinationY, destinationZ);
+        this.docStartPosition = docStartPosY;
+        this.docEndPosition = endPositionY;
+        // this.destinationV = new V3(destinationX, destinationY, destinationZ);
+        this.destinationV = new Three.Vector3(destinationX, destinationY, destinationZ);
+        // console.log('vector3', this.destinationV)
         this.destinationAngleDeg = angleDeg;
         this.angleRad = angleDeg * Math.PI / 180;
     }
 }
 
-export class V3 {
-    x: number;
-    y: number;
-    z: number;
-    constructor(x: number, y: number, z: number) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-}
+// export class V3 {
+//     x: number;
+//     y: number;
+//     z: number;
+//     constructor(x: number, y: number, z: number) {
+//         this.x = x;
+//         this.y = y;
+//         this.z = z;
+//     }
+// }
