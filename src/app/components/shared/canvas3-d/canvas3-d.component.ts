389df7f7d204105d06f72d300022ad0a16daf446
Yordi David <yordinosaure@gmail.com>
2020-10-12 15:11:50 +0200

    canvas 3d refacto

--- src/app/components/shared/canvas3-d/canvas3-d.component.ts
+++ src/app/components/shared/canvas3-d/canvas3-d.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular
 import { ThreeDService } from 'src/app/core/services/three-d.service';
 import { ThreeService } from 'src/app/core/services/three/three.service';
 import { DataExchangeService } from 'src/app/core/services/DataExchange/data-exchange.service';
-import { Observable, observable } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { Vector3 } from 'three';
+import { DisplayData } from 'src/app/core/models/displayData.model';
+import { ScrollingService } from 'src/app/core/services/scrolling/scrolling.service';
 
 @Component({
   selector: 'app-canvas3-d',
@@ -19,102 +23,25 @@ export class Canvas3DComponent implements OnInit {
     this.threeDService.resizeAction();
   }
 
-  @HostListener('mousemove', ['$event'])
-  onMouseDrag(event) {
-    // this.threeService.onMouseDragAction(event);
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event) {
+    // event.preventDefault();
+    // console.log('scrolling')
   }
-
-  // @HostListener('wheel', ['$event']) // for window wheel events
-  // onWheel(event) {
-  //  console.log(event.deltaY)
-  //   this.threeService.zoom(event.deltaY);
-  // }
-
-  // @HostListener('window:scroll', ['$event'])
-  // onScroll(event) {
-  //   event.preventDefault();
-  // }
 
   @HostListener('wheel', ['$event'])
   onWheel(event) {
-    //  this.threeDService.upDateDisplayCoord(window.pageYOffset);
-    //  console.log('scroll', window.pageYOffset)
-    // this.getActualPos()
-    if (!this.scrolling) {
-      this.scroll(event.deltaY)
-
-    }
-    // console.log(event)
+    // console.log('wheel')
+      this.scrollingService.scroll(event.deltaY);
   }
 
-  offsetData: any[] = [];
-  currentOffsetData: number;
-  scrollY = 0;
-  scrolling = false;
-  endY: number;
-  startY: number;
-  actualY: number = 1;
-
-  constructor(private threeDService: ThreeDService, private dataEx: DataExchangeService
+ 
+  constructor(private threeDService: ThreeDService, private dataEx: DataExchangeService, private scrollingService: ScrollingService
     // private threeService: ThreeService
   ) { }
 
   ngOnInit(): void {
     this.threeDService.createScene(this.rendererCanvas);
-    this.dataEx.componentsData.subscribe(x => {
-      this.offsetData = x;
-      this.startY = this.offsetData[0];
-      this.endY = this.offsetData[1];
-      this.getActualPos();
-    })
+  } 
 
-  }
-
-  getActualPos() {
-    this.actualY = document.documentElement.scrollTop;
-    if (this.actualY > this.endY || this.actualY < this.startY) {
-
-      for (let i = 0; i < this.offsetData.length; i++) {
-        if (this.actualY >= this.offsetData[i] && this.actualY < this.offsetData[i + 1]) {
-          this.startY = this.offsetData[i];
-          this.endY = this.offsetData[i + 1];
-        }
-      }
-    }
-  }
-
-  scroll(deltaY: number): void {
-    console.log('event', event)
-    this.getActualPos();
-    this.scrolling = true;
-    if (deltaY >= 0) {
-      console.log('if')
-      window.scrollTo({
-        top: this.scrollY += 5,
-        left: 0,
-        behavior: 'smooth'
-      });
-      console.log('actual', this.actualY)
-      console.log('end', this.endY);
-      console.log('scrollY', this.scrollY);
-      if (this.scrollY < this.endY) {
-        setTimeout(() => { this.scroll(deltaY) }, 10)
-      }
-      else {
-        this.scrolling = false;
-      }
-    } 
-    else {
-      window.scrollTo({
-        top: this.scrollY -= 5,
-        left: 0,
-        behavior: 'smooth'
-      });
-      if (this.scrollY > this.startY) {
-        setTimeout(() => { this.scroll(deltaY) }, 10)
-      } else {
-        this.scrolling = false;
-      }
-    }
-  }
 }
